7b8088252f6c4b6cc11710718ef520ea353903d6
Eric Cool <coolazura@gmail.com>
2019-07-21 13:13:56 -0400

    Switched from .ini to .json config file namelist and tweaked the look of the code.

--- CustomNameList/CustomNameList.cs
+++ CustomNameList/CustomNameList.cs
@@ -7,46 +7,25 @@ using System.IO;
 using Newtonsoft.Json;
 using Harmony;
 
-namespace CustomNameList
-{
+namespace CustomNameList {
     [HarmonyPatch(typeof(CharacterContainer), "SetInfoText")]
-    public class CustomNameList
-    {
+    public class CustomNameList{
         public static NameList names;
 		public static string path;
 		public static string filepath;
-
-		public static IniFile config2;
+		public static string filename = "namelists.json";
 		public static Config config;
 
-        public static void Prefix(CharacterContainer __instance)
-        {
+        public static void Prefix(CharacterContainer __instance){
 			if(path == null){
 				path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase).Replace("file:\\", "");
-				filepath = Path.Combine(path, "namelists.json");
+				filepath = Path.Combine(path, filename);
 			}
 
-			// Only use mod if namelists.txt exists
+			// Only use mod if config file exists
 			if(File.Exists(filepath)){
-				if(config == null){
-					
-					//Debug.Log(config.ReadConfig());
-				}
-				/*if(config2 == null){
-					Debug.Log("--- Creating Config2 IniFile ---");
-					config2 = new IniFile(filepath);
-					Debug.Log(config2);
-				}
-
-				if(names == null){
-					Debug.Log("--- Creating NameList ---");
-					names = new NameList(config2);
-					Debug.Log(names);
-				}*/
-
-				if(names == null){
-
-				}
+				if(config == null) config = new Config(path, filename);
+				if(names == null) names = new NameList(config);
 
 				// new List<MinionIdentity>(Components.LiveMinionIdentities.Items); // NOTE: Maybe eventually track other dupe names and limit new name unless all used.
 				var stats = Traverse.Create(__instance).Property("Stats");
@@ -55,7 +34,7 @@ namespace CustomNameList
 
 				var old_name = name.GetValue();
 
-				//name.SetValue(names.Next(gender_val));
+				name.SetValue(names.Next(gender_val));
 
 				Debug.Log("Name changed from `" + old_name + "` => `" + name.GetValue() + "`");
 				
@@ -63,39 +42,45 @@ namespace CustomNameList
         }
     }
 
-    public class NameList
-	{
-		/*
-		Modified from ONI - MinionIdentities.NameList
-		*/
+    public class NameList{
+		// REF: ONI - MinionIdentities.NameList
 		public Dictionary<string, List<string>> names = new Dictionary<string, List<string>>();
 		public Dictionary<string, int> idx = new Dictionary<string, int>();
-
 		public List<string> genders = new List<string>{ "MALE", "FEMALE", "NB" };
 
-		public NameList(IniFile config)
-		{
+		public NameList(Config config){
 			foreach(string gender in genders){
 				Debug.Log("--- Populating NameList for " + gender + " ---");
 				// create default index for each Gender
 				this.idx.Add(gender, 0);
 
-				List<string> tmp = new List<string>(config.Read(gender, "Names").Split(','));
-				this.names.Add(gender, tmp);
+				// get `gender` key from Config and add to the `gender` NameList
+				this.names.Add(gender, config.Get(gender));
 
 				// shuffle each NameList
 				this.names[gender].Shuffle<string>();
 			}
 		}
 
-		public string Next(String gender)
-		{
+		public string Next(string gender){
 			return this.names[gender][this.idx[gender]++ % this.names[gender].Count];
 		}
 	}
 
-	public class Config
-	{
+	public class Config{
+		// TODO: separate into a helper lib and expand writing capabilities
+		public string json;
+		public Dictionary<string, List<string>> keys;
 
+		public Config(string path, string filename){
+			using (StreamReader file = new StreamReader(Path.Combine(path, filename))){
+				this.json = file.ReadToEnd();
+				this.keys = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(json);
+			}
+		}
+
+		public List<string> Get(string key){
+			return this.keys[key];
+		}
 	}
 }

851807e60553853b9f1df597138e92b8c4445936
Eric Cool <coolazura@gmail.com>
2019-08-01 11:38:23 -0400

    fix assembly name, attempt to fix odd indentation on gitea

--- FavoritesCategory/Properties/AssemblyInfo.cs
+++ FavoritesCategory/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PROJECT_NAME")]
+[assembly: AssemblyTitle("FavoritesCategory")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PROJECT_NAME")]
+[assembly: AssemblyProduct("FavoritesCategory")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

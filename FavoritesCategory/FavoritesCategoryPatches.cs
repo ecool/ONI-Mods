0e92e66dae91967d5ea388857f9c6d9980ce31f8
Eric Cool <coolazura@gmail.com>
2019-08-02 15:26:37 -0400

    tidy up code, prepare for steam release

--- FavoritesCategory/FavoritesCategoryPatches.cs
+++ FavoritesCategory/FavoritesCategoryPatches.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using System.IO;
+using UnityEngine;
 using Harmony;
 
 namespace FavoritesCategory {
 	public class FavoritesCategoryPatches{
 		public static ResourceCategoryHeader favoritesCategoryHeader = null;
-		// Global.GameInputManager - class GameInputManager : KInputManager
-		// KInputController KInputManager.GetDefaultController()
-		// KInputController.GetKeyDown(KKeyCode key_code)
 
 		[HarmonyPatch(typeof(ResourceEntry), "OnClick")]
 		public static class ResourceEntry_OnClick_Patch
@@ -25,21 +23,35 @@ namespace FavoritesCategory {
 
 				if(check == Modifier.Shift){
 					cont = false; // don't execute original OnClick
-					// __instance.Resource
-					/* Example output:
-					[04:59:42.160] [1] [INFO] Dirt
-					[04:59:44.123] [1] [INFO] Dirttag2.Name
-					[04:59:45.495] [1] [INFO] Sand
-					[04:59:46.878] [1] [INFO] SandStone
-					[04:59:47.607] [1] [INFO] Cuprite
-					[04:59:49.185] [1] [INFO] Algae
-					*/
+
 					Tag tag = (Tag) __instance.Resource;
-					Debug.Log(tag);
-					Debug.Log(favoritesCategoryHeader);
+
 					if (favoritesCategoryHeader != null) {
-						var discoverCat = Traverse.Create(WorldInventory.Instance).Method("DiscoverCategory", new[] { typeof(Tag), typeof(Tag) });
-						discoverCat.GetValue(favoritesCategoryHeader.ResourceCategoryTag, tag);
+						ResourceEntry resourceEntry = null;
+						favoritesCategoryHeader.ResourcesDiscovered.TryGetValue(tag, out resourceEntry);
+
+						HashSet<Tag> hashSet = null;
+						if (WorldInventory.Instance.TryGetDiscoveredResourcesFromTag(favoritesCategoryHeader.ResourceCategoryTag, out hashSet)) {
+							if (hashSet.Contains(tag)) {
+								// Toggle Off
+								// NOTE: removes resource tracking in Favorites category but still displays the button
+								hashSet.Remove(tag);
+
+								if (resourceEntry != null) {
+									resourceEntry.gameObject.SetActive(false);
+								}
+							}else{
+								// Toggle On
+								// Activate the ResourceEntry
+								var discoverCat = Traverse.Create(WorldInventory.Instance).Method("DiscoverCategory", new[] { typeof(Tag), typeof(Tag) });
+								discoverCat.GetValue(favoritesCategoryHeader.ResourceCategoryTag, tag);
+
+								if (resourceEntry != null) {
+									resourceEntry.gameObject.SetActive(true);
+								}
+							}
+						}
+
 					}
 
 				}
@@ -57,37 +69,28 @@ namespace FavoritesCategory {
 				// Create Favorites tag
 				Tag favoritesTag = TagManager.Create("Favorites", "Favorites");
 
-
+				// Create Favorites Header
 				var createTagSetHeaders = Traverse.Create(__instance).Method("CreateTagSetHeaders", new[] { typeof(IEnumerable<Tag>), typeof(GameUtil.MeasureUnit) });
 				createTagSetHeaders.GetValue(new TagSet{ favoritesTag }, GameUtil.MeasureUnit.mass);
 
-				var displayedCategories = Traverse.Create(__instance).Field("DisplayedCategoryKeys");
-				// TODO: figure out how to order `Favorites` on top of list
-				//var tags = new List<Tag>((Tag[]) displayedCategories.GetValue());
-				//tags.Insert(0, favoritesTag);
-				//displayedCategories.SetValue(tags.ToArray());
-				displayedCategories.SetValue(__instance.DisplayedCategories.Keys.ToArray<Tag>());
-				//private Tag[] DisplayedCategoryKeys
-
+				// reset DisplayedCategories, so Favorites is displayed
+				var displayedCategoryKeys = Traverse.Create(__instance).Field("DisplayedCategoryKeys");
+				displayedCategoryKeys.SetValue(__instance.DisplayedCategories.Keys.ToArray<Tag>());
 
 				// keep our instance of favoritesCategoryHeader saved
 				__instance.DisplayedCategories.TryGetValue(favoritesTag, out favoritesCategoryHeader);
 				Debug.Log(favoritesCategoryHeader);
 
-			}
-		}
+				// move Favorites to the top of the list
+				favoritesCategoryHeader.transform.SetAsFirstSibling();
 
-		/* WorldInventory
-		private void DiscoverCategory(Tag category_tag, Tag item_tag)
-		{
-			HashSet<Tag> hashSet;
-			if (!this.DiscoveredCategories.TryGetValue(category_tag, out hashSet))
-			{
-				hashSet = new HashSet<Tag>();
-				this.DiscoveredCategories[category_tag] = hashSet;
+				// TODO: add customizable color
+				// NOTE: below doesn't work
+				//favoritesCategoryHeader.TextColor_Interactable = new Color(85f, 181f, 219f);
+				//favoritesCategoryHeader.elements.LabelText.textStyleSetting.textColor = favoritesCategoryHeader.TextColor_Interactable;
+				//Traverse.Create(favoritesCategoryHeader).Method("SetActiveColor", new[] { typeof(bool) }).GetValue(true);
+
 			}
-			hashSet.Add(item_tag);
 		}
-		*/
 	}
 }

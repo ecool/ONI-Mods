f44f657f02f771afc95e4ed3c42cbca1156ac1aa
Eric Cool <coolazura@gmail.com>
2021-01-30 14:22:43 -0500

    fix formatting, add tile to building menu and research

--- BuildableNaturalTile/BuildableNaturalTileConfig.cs
+++ BuildableNaturalTile/BuildableNaturalTileConfig.cs
@@ -4,59 +4,57 @@ using TUNING;
 using UnityEngine;
 using STRINGS;
 
-public class NaturalTileConfig : IBuildingConfig
-{
+public class NaturalTileConfig : IBuildingConfig {
     public const string ID = "NaturalTile";
     public const string DisplayName = "Natural Tile";
     public const string Description = "Fill that hole you dug out back in with any solid element.";
     public static string Effect = $"Fills a block in the world with {UI.FormatAsLink("Solids", "ELEMENTS_SOLID")}.";
 
-	public static readonly int BlockTileConnectorID = Hash.SDBMLower("natural_tile_block");
+    public static readonly int BlockTileConnectorID = Hash.SDBMLower("natural_tile_block");
 
-	public override BuildingDef CreateBuildingDef()
-	{
+    public override BuildingDef CreateBuildingDef() {
         string id = "NaturalTile";
-		int width = 1;
-		int height = 1;
-		string anim = "natural_tile_kanim";
-		int hitpoints = 100;
-		float construction_time = BuildableNaturalTile.BuildableNaturalTilePatches.Settings.BuildSpeed;
-		float[] tier = new float[] { BuildableNaturalTile.BuildableNaturalTilePatches.Settings.BuildMass }; // 50kg
-		string[] raw_MINERALS = new string[] {"Solid"}; // currently set to all solids
-		float melting_point = 1600f;
-		BuildLocationRule build_location_rule = BuildLocationRule.Anywhere;
-		EffectorValues none = NOISE_POLLUTION.NONE;
-		BuildingDef buildingDef = BuildingTemplates.CreateBuildingDef(id, width, height, anim, hitpoints, construction_time, tier, raw_MINERALS, melting_point, build_location_rule, TUNING.BUILDINGS.DECOR.BONUS.TIER0, none, 0.2f);
-		BuildingTemplates.CreateFoundationTileDef(buildingDef);
-		buildingDef.Floodable = false;
-		buildingDef.Entombable = false;
-		buildingDef.Overheatable = false;
-		buildingDef.ForegroundLayer = Grid.SceneLayer.BuildingBack;
-		buildingDef.AudioCategory = "HollowMetal";
-		buildingDef.AudioSize = "small";
-		buildingDef.BaseTimeUntilRepair = -1f;
-		buildingDef.SceneLayer = Grid.SceneLayer.TileMain;
-		buildingDef.ConstructionOffsetFilter = BuildingDef.ConstructionOffsetFilter_OneDown;
-		buildingDef.isSolidTile = false;
-		buildingDef.DragBuild = true;
-		buildingDef.ObjectLayer = ObjectLayer.Building;
-		return buildingDef;
-	}
+        int width = 1;
+        int height = 1;
+        string anim = "natural_tile_kanim";
+        int hitpoints = 100;
+        float construction_time = BuildableNaturalTile.BuildableNaturalTilePatches.Settings.BuildSpeed;
+        float[] tier = new float[] { BuildableNaturalTile.BuildableNaturalTilePatches.Settings.BuildMass }; // 50kg
+        string[] raw_MINERALS = new string[] { "Solid" }; // currently set to all solids
+        float melting_point = 1600f;
+        BuildLocationRule build_location_rule = BuildLocationRule.Anywhere;
+        EffectorValues none = NOISE_POLLUTION.NONE;
+        BuildingDef buildingDef = BuildingTemplates.CreateBuildingDef(
+            id, width, height, anim, hitpoints, construction_time, tier,
+            raw_MINERALS, melting_point, build_location_rule,
+            TUNING.BUILDINGS.DECOR.BONUS.TIER0, none, 0.2f);
+        BuildingTemplates.CreateFoundationTileDef(buildingDef);
+        buildingDef.Floodable = false;
+        buildingDef.Entombable = false;
+        buildingDef.Overheatable = false;
+        buildingDef.ForegroundLayer = Grid.SceneLayer.BuildingBack;
+        buildingDef.AudioCategory = "HollowMetal";
+        buildingDef.AudioSize = "small";
+        buildingDef.BaseTimeUntilRepair = -1f;
+        buildingDef.SceneLayer = Grid.SceneLayer.TileMain;
+        buildingDef.ConstructionOffsetFilter = BuildingDef.ConstructionOffsetFilter_OneDown;
+        buildingDef.isSolidTile = false;
+        buildingDef.DragBuild = true;
+        buildingDef.ObjectLayer = ObjectLayer.Building;
+        return buildingDef;
+    }
 
-	public override void ConfigureBuildingTemplate(GameObject go, Tag prefab_tag)
-	{
-		GeneratedBuildings.MakeBuildingAlwaysOperational(go);
-		BuildingConfigManager.Instance.IgnoreDefaultKComponent(typeof(RequiresFoundation), prefab_tag);
-	}
+    public override void ConfigureBuildingTemplate(GameObject go, Tag prefab_tag) {
+        GeneratedBuildings.MakeBuildingAlwaysOperational(go);
+        BuildingConfigManager.Instance.IgnoreDefaultKComponent(typeof(RequiresFoundation), prefab_tag);
+    }
 
-	public override void DoPostConfigureComplete(GameObject go)
-	{
-		GeneratedBuildings.RemoveLoopingSounds(go);
-		go.GetComponent<KPrefabID>().AddTag(GameTags.FloorTiles, false);
-	}
+    public override void DoPostConfigureComplete(GameObject go) {
+        GeneratedBuildings.RemoveLoopingSounds(go);
+        go.GetComponent<KPrefabID>().AddTag(GameTags.FloorTiles, false);
+    }
 
-	public override void DoPostConfigureUnderConstruction(GameObject go)
-	{
-		base.DoPostConfigureUnderConstruction(go);
-	}
+    public override void DoPostConfigureUnderConstruction(GameObject go) {
+        base.DoPostConfigureUnderConstruction(go);
+    }
 }
--- BuildableNaturalTile/BuildableNaturalTilePatches.cs
+++ BuildableNaturalTile/BuildableNaturalTilePatches.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 using Harmony;
 using UnityEngine;
 using CaiLib.Utils;
@@ -14,27 +15,31 @@ namespace BuildableNaturalTile {
         public static void OnLoad() => LogInit("BNT"); // Initialize mod logs <CoolLib>
 
         [HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
-        public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
-        {
-            public static void Prefix()
-            {
-                AddBuildingStrings(NaturalTileConfig.ID, NaturalTileConfig.DisplayName, NaturalTileConfig.Description, NaturalTileConfig.Effect);
-                AddBuildingToPlanScreen(GameStrings.PlanMenuCategory.Base, NaturalTileConfig.ID);
+        public static class GeneratedBuildings_LoadGeneratedBuildings_Patch {
+            public static void Prefix() {
+                AddBuildingStrings(NaturalTileConfig.ID, NaturalTileConfig.DisplayName,
+                    NaturalTileConfig.Description, NaturalTileConfig.Effect);
+                int index = TUNING.BUILDINGS.PLANORDER.FindIndex(x => x.category == "Base");
+                if (index == -1)
+                    return;
+
+                // add tile to `Base` building tab
+                IList<string> planOrderList = TUNING.BUILDINGS.PLANORDER[index].data;
+                planOrderList.Add(NaturalTileConfig.ID);
             }
         }
 
-        [HarmonyPatch(typeof(Db), "Initialize")]
-        public static class Db_Initialize_Patch
-        {
-            public static void Prefix()
-            {
-                AddBuildingToTechnology(GameStrings.Technology.Food.BasicFarming, NaturalTileConfig.ID);
+        [HarmonyPatch(typeof(Database.Techs), "Init")]
+        public static class Database_Techs_Init_Patch {
+            public static void Postfix(ref Database.Techs __instance) {
+                // Add to `Basic Farming` technology
+                Tech techBasicFarming = __instance.TryGetTechForTechItem("RationBox");
+                techBasicFarming.unlockedItemIDs.Add(NaturalTileConfig.ID);
             }
         }
 
         [HarmonyPatch(typeof(BuildingComplete), "OnSpawn")]
-        public static class BuildingComplete_OnSpawn_Patch
-        {
+        public static class BuildingComplete_OnSpawn_Patch {
             private static readonly CellOffset[] _displacementOffsets = new CellOffset[]
             {
                 new CellOffset(0, 1),
@@ -47,10 +52,9 @@ namespace BuildableNaturalTile {
                 new CellOffset(-1, -1)
             };
 
-            public static void Postfix(BuildingComplete __instance)
-            {
+            public static void Postfix(BuildingComplete __instance) {
                 GameObject go = __instance.gameObject;
-                if(__instance.name == "NaturalTileComplete"){
+                if (__instance.name == "NaturalTileComplete") {
                     LogDebug($"__instance.name: {__instance.name}");
 
                     Vector3 pos = go.transform.position;
@@ -58,29 +62,52 @@ namespace BuildableNaturalTile {
                     float temperature = element.Temperature;
                     float mass = Settings.BlockMass;
                     int cell = Grid.PosToCell(pos);
-                    SimMessages.ReplaceAndDisplaceElement(cell, element.ElementID, null, mass, temperature, byte.MaxValue, 0, -1); // spawn Dirt Block
+                    LogDebug($"{element}");
 
                     LogDebug($"pos: {pos}, temperature: {temperature}, cell: {cell}");
 
+                    // SimMessages.ReplaceAndDisplaceElement(
+                    //     int gameCell, SimHashes new_element, CellElementEvent ev,
+                    //     float mass, float temperature = -1f, byte disease_idx = 255,
+                    //     int disease_count = 0, int callbackIdx = -1
+                    // )
+                    SimMessages.ReplaceAndDisplaceElement(cell, element.ElementID,
+                        null, mass, temperature, byte.MaxValue, 0, -1); // spawn Natural Block
+
+                    // Error randomly showing up after creation of natural block
+                    // NullReferenceException: Object reference not set to an instance of an object
+                    // at ObjectLayerListItem.Refresh(System.Int32 new_cell)[0x00149]
+                    //      in C:\jenkins_workspace\workspace\SimGame_Windows\game\Assets\
+                    //      scripts\game\ObjectLayerListItem.cs:59
+                    // at ObjectLayerListItem.Update(System.Int32 cell)[0x00001]
+                    //      in C:\jenkins_workspace\workspace\SimGame_Windows\game\Assets\
+                    //      scripts\game\ObjectLayerListItem.cs:72
+                    // at Pickupable.OnCellChange()[0x000d1] in C:\jenkins_workspace\workspace\
+                    //      SimGame_Windows\game\Assets\scripts\components\Pickupable.cs:479
+                    // at CellChangeMonitor.RenderEveryTick()[0x00143] in C:\jenkins_workspace\
+                    //      workspace\SimGame_Windows\game\Assets\Plugins\Klei\util\
+                    //      CellChangeMonitor.cs:194
+                    // at Game.Update()[0x00091] in C:\jenkins_workspace\workspace\SimGame_Windows\
+                    //      game\Assets\scripts\game\Game.cs:1341
+
                     // NOTE: Displace pickupables needs to be watched for possibly slowing down the game during late game
                     int origCell = cell;
-                    foreach (Pickupable pickupable in Components.Pickupables){
-                        if (Grid.PosToCell(pickupable) == origCell){
+                    foreach (Pickupable pickupable in Components.Pickupables) {
+                        if (Grid.PosToCell(pickupable) == origCell) {
                             LogDebug($"pickupable: {pickupable}");
 
-                            for (int i=0; i<_displacementOffsets.Length; i++){
+                            for (int i = 0; i < _displacementOffsets.Length; i++) {
                                 int offsetCell = Grid.OffsetCell(cell, _displacementOffsets[i]);
-                                if (Grid.IsValidCell(offsetCell) && !Grid.Solid[offsetCell]){
+                                if (Grid.IsValidCell(offsetCell) && !Grid.Solid[offsetCell]) {
                                     Vector3 position = Grid.CellToPosCBC(offsetCell, Grid.SceneLayer.Move);
                                     KCollider2D component = pickupable.GetComponent<KCollider2D>();
-                                    if (component != null)
-                                    {
+                                    if (component != null) {
                                         position.y += pickupable.transform.GetPosition().y - component.bounds.min.y;
                                     }
                                     pickupable.transform.SetPosition(position);
                                     cell = offsetCell;
 
-                                    Traverse.Create(pickupable).Method("RemoveFaller", new Type[] {}).GetValue();
+                                    Traverse.Create(pickupable).Method("RemoveFaller", new Type[] { }).GetValue();
                                     Traverse.Create(pickupable).Method("AddFaller", new Type[] { typeof(Vector2) }).GetValue(Vector2.zero);
 
                                     break;
@@ -89,7 +116,7 @@ namespace BuildableNaturalTile {
                         }
                     }
 
-                    go.DeleteObject(); // remove Dirt Tile
+                    go.DeleteObject(); // remove Natural Tile
                 }
             }
         }
